971c55f0aad53c141d5cbbf639431a19f5883ac6
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2025-02-23 14:20:39 +0800

    Initialize Phaser project with basic game setup and assets

--- README.md
+++
@@ -1,1 +0,0 @@
-# phaserjs_pratice
---
+++ src/main.js
@@ -0,0 +1,86 @@
+import "./style.css";
+import Phaser, { Physics, Scene } from "phaser";
+
+const sizes = {
+  width: 500,
+  height: 500,
+};
+
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+    this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
+    this.target;
+  }
+
+  preload() {
+    this.load.image("bg", "assets/bg.png");
+    this.load.image("basket", "assets/basket.png");
+    this.load.image("apple", "assets/apple.png");
+  }
+
+  create() {
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+    this.player = this.physics.add
+      .image(0, sizes.height - 100, "basket")
+      .setOrigin(0, 0);
+    this.player.setCollideWorldBounds(true);
+
+    this.target = this.physics.add.image(0, 0, "apple").setOrigin(0, 0);
+    this.target.setMaxVelocity(0, speedDown);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(
+      this.player,
+      this.target,
+      this.targetHit,
+      null,
+      this
+    );
+  }
+
+  update() {
+    if (this.target.y > sizes.height) {
+      this.target.y = 0;
+      this.target.setX(Phaser.Math.Between(0, sizes.width - this.target.width));
+    }
+
+    const { left, right } = this.cursor;
+
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  }
+
+  targetHit() {
+    target.setY(0);
+    target.setX(Phaser.Math.Between(0, sizes.width - target.width));
+    this.points++;
+  }
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
